43b78eadbee22cd4710626332d3a00c661420051
stubbornnessness <1219721814yxj>
2016-10-01 01:05:11 +0800

    1.1版本支持Carthage

---
+++ YXJOnePixeLineTest/YXJOnePixeLine/YXJOnePixeLine.h
@@ -0,0 +1,19 @@
+//
+//  YXJOnePixeLine.h
+//  YXJOnePixeLine
+//
+//  Created by yuanxiaojun on 2016/10/1.
+//  Copyright © 2016年 袁晓钧. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YXJOnePixeLine.
+FOUNDATION_EXPORT double YXJOnePixeLineVersionNumber;
+
+//! Project version string for YXJOnePixeLine.
+FOUNDATION_EXPORT const unsigned char YXJOnePixeLineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YXJOnePixeLine/PublicHeader.h>
+
+
--- YXJOnePixeLineTest/YXJOnePixeLineTest/ViewController.swift
+++ YXJOnePixeLineTest/YXJOnePixeLineTest/ViewController.swift
@@ -17,32 +17,32 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
 
-    @IBAction func click(sender: AnyObject) {
+    @IBAction func click(_ sender: AnyObject) {
         // linePosition的按照顺时针的方向，依次对应 上>右>下>左
 
         // 上
         let pixe1 = YXJOnePixelLine(frame: onePixeView.frame,
-                                    lineColor: UIColor.redColor(),
+                                    lineColor: UIColor.red,
                                     linePosition: 0)
-        self.onePixeView.addSubview(pixe1)
+        self.onePixeView.addSubview(pixe1!)
 
         // 右
         let pixe2 = YXJOnePixelLine(frame: onePixeView.frame,
-                                    lineColor: UIColor.redColor(),
+                                    lineColor: UIColor.red,
                                     linePosition: 1)
-        self.onePixeView.addSubview(pixe2)
+        self.onePixeView.addSubview(pixe2!)
         
         // 下
         let pixe3 = YXJOnePixelLine(frame: onePixeView.frame,
-                                    lineColor: UIColor.redColor(),
+                                    lineColor: UIColor.red,
                                     linePosition: 2)
-        self.onePixeView.addSubview(pixe3)
+        self.onePixeView.addSubview(pixe3!)
         
         // 左
         let pixe4 = YXJOnePixelLine(frame: onePixeView.frame,
-                                    lineColor: UIColor.redColor(),
+                                    lineColor: UIColor.red,
                                     linePosition: 3)
-        self.onePixeView.addSubview(pixe4)
+        self.onePixeView.addSubview(pixe4!)
     }
 
     override func didReceiveMemoryWarning() {
